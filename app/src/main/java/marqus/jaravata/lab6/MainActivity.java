8c5183aec22af2bb1d19b3aeed20a8bbad9aa219
marqus <jaravatamarqus@rocketmail.com>
2023-03-17 06:57:05 -0400

    Implemented bottom_navigation_view.xml : Add the bottom_navigation_view.xml to the layout files and added in the necessary fragments. Implemented the MainActivity to handle navigation between the fragments using switch statements and listeners.

---
+++ app/src/main/java/marqus/jaravata/lab6/MainActivity.java
@@ -0,0 +1,83 @@
+package marqus.jaravata.lab6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_navigation_view);
+
+        // Initialize the BottomNavigationView and set the listener
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        // Initialize the Toolbar and set the menu item click listener
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(menuItemClickListener);
+
+        // Set the default fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+    }
+
+    // Listener for BottomNavigationView clicks
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_john:
+                    selectedFragment = new JohnFragment();
+                    break;
+                case R.id.nav_person:
+                    selectedFragment = new PersonFragment();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+
+            // Load the selected fragment
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
+        }
+    };
+
+    // Listener for Toolbar menu item clicks
+    private Toolbar.OnMenuItemClickListener menuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_settings:
+                    // Open the device settings screen
+                    Intent settingsIntent = new Intent(Settings.ACTION_SETTINGS);
+                    startActivity(settingsIntent);
+                    return true;
+            }
+            return false;
+        }
+    };
+}
+
