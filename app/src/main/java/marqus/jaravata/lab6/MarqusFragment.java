951bc4f7adea77237cda211e888e3dbbe7cd432c
marqus <jaravatamarqus@rocketmail.com>
2023-03-17 17:29:55 -0400

    Implemented in MarqusFragment.java: set an OnCheckedChangeListener for the RadioGroup that shows an AlertDialog when a radio button is selected. The showAlertDialog() method creates and shows the AlertDialog with a title, message, and a single "OK" button. Implemented in fragment_marqus.xml. : Defined  RelativeLayout that contains a TextView and a RadioGroup. The TextView displays the user's first name in bold blue text that is centered vertically and horizontally on the screen. The RadioGroup contains three radio buttons that are displayed vertically below the TextView.

--- app/src/main/java/marqus/jaravata/lab6/MarqusFragment.java
+++ app/src/main/java/marqus/jaravata/lab6/MarqusFragment.java
@@ -1,4 +1,54 @@
 package marqus.jaravata.lab6;
 
-public class MarqusFragment {
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MarqusFragment extends Fragment {
+
+    private RadioGroup radioGroup;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_marqus, container, false);
+
+        radioGroup = view.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+                showAlertDialog(radioButton.getText().toString());
+            }
+        });
+
+        return view;
+    }
+
+    private void showAlertDialog(String selectedLabel) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.drawable.ic_alert_icon);
+        builder.setTitle(getString(R.string.full_name));
+        builder.setMessage(selectedLabel);
+        builder.setPositiveButton(getString(R.string.ok_text), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+    }
 }
+
