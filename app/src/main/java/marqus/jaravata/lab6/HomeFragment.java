dd541405f037ba5db42ec098dbf74fbcdc13d196
marqus <jaravatamarqus@rocketmail.com>
2023-03-17 16:50:28 -0400

    Implemented in fragment_home.xml : Progress bar and imageButton. Implemented the HomeFragment.java with the methods for progrssBar and ImageButton. Added in string options and translated.

--- app/src/main/java/marqus/jaravata/lab6/HomeFragment.java
+++ app/src/main/java/marqus/jaravata/lab6/HomeFragment.java
@@ -1,6 +1,44 @@
 package marqus.jaravata.lab6;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
+    private ProgressBar progressBar;
+    private ImageButton imageButton;
+    private int counter = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        progressBar = view.findViewById(R.id.progress_bar);
+        imageButton = view.findViewById(R.id.image_button);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                String message = getString(R.string.first_name) + counter;
+                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+
+                if (counter == 5) {
+                    progressBar.setVisibility(View.GONE);
+                } else if (counter == 8) {
+                    getActivity().finish();
+                }
+            }
+        });
+
+        return view;
+    }
 }
+
