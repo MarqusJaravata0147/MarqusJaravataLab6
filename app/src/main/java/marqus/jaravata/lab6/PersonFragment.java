2033bd92adfa0168c697ea031006c44e0cd346b3
marqus <jaravatamarqus@rocketmail.com>
2023-03-17 17:38:37 -0400

    Implemented in fragment_person.xml: In the above code, we define a LinearLayout that contains a ListView. The ListView is given an ID of list_view and is set to have a choiceMode of singleChoice, which allows the user to select only one item at a time. The background attribute sets the background color of the ListView. Implemented in PersonFragment.java.:  initialize an array of provinces by getting the string array resource from strings.xml. We create an ArrayAdapter with a simple list item layout and set it as the adapter for the ListView. We also set the CHOICE_MODE_SINGLE for the ListView and set an OnItemClickListener to handle item selection. When an item is selected, we store the selected index and province in the MainActivity using public methods.

--- app/src/main/java/marqus/jaravata/lab6/PersonFragment.java
+++ app/src/main/java/marqus/jaravata/lab6/PersonFragment.java
@@ -1,4 +1,44 @@
 package marqus.jaravata.lab6;
 
-public class PersonFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PersonFragment extends Fragment {
+
+    private ListView listView;
+    private String[] provinces;
+    private int selectedIndex = -1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
+
+        listView = view.findViewById(R.id.list_view);
+        provinces = getResources().getStringArray(R.array.provinces_and_territories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_single_choice, provinces);
+        listView.setAdapter(adapter);
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedIndex = position;
+                ((MainActivity)getActivity()).setSelectedIndex(selectedIndex);
+                ((MainActivity)getActivity()).setSelectedProvince(provinces[selectedIndex]);
+            }
+        });
+
+        return view;
+    }
 }
+
